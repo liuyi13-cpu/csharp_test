40cdb206267314ec471b2efab8614d753749f5b1
wangxueping03 <wangxueping03@kuaishou.com>
2024-08-18 17:43:00 +0800

    数组字典遍历性能测试

--- Assets/Scripts/TestArray.cs
+++ Assets/Scripts/TestArray.cs
@@ -1,19 +1,21 @@
+using System.Collections.Generic;
 using System.Diagnostics;
 using Debug = UnityEngine.Debug;
 
-/// <summary>
-/// 二维数组性能测试
-/// [] vs [,]
-/// 结论：一维数组性能更好点
-/// [DEBUG] 1560ms vs 1960ms
-/// </summary>
 public class TestArray : ITest
 {
     public void DoTest()
     {
         Test1();
+        Test2();
     }
     
+    /// <summary>
+    /// 二维数组性能测试
+    /// [] vs [,]
+    /// 结论：一维数组性能更好点
+    /// [DEBUG] 1560ms vs 1960ms
+    /// </summary>
     private void Test1()
     {
         int rows = 1000;
@@ -51,5 +53,48 @@ public class TestArray : ITest
         }
         sw2.Stop();
         Debug.LogWarning($"2D array time: {sw2.ElapsedMilliseconds} ms");
+    }
+
+    /// <summary>
+    /// 数组字典遍历性能测试
+    /// 结论：数组性能好  10倍
+    /// 195ms vs 1728ms
+    /// </summary>
+    private void Test2()
+    {
+        // 1.数组
+        int size = 1000000*100;
+        int[] array = new int[size];
+        // 初始化数组
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = i;
+        }
+        // 测量遍历数组的时间
+        Stopwatch sw = Stopwatch.StartNew();
+        long sum = 0;
+        for (int i = 0; i < size; i++)
+        {
+            sum += array[i];
+        }
+        sw.Stop();
+        Debug.LogWarning($"遍历数组耗时: {sw.ElapsedMilliseconds} ms");
+
+        // 2.字典
+        Dictionary<int, int> dictionary = new Dictionary<int, int>();
+        // 初始化字典
+        for (int i = 0; i < size; i++)
+        {
+            dictionary[i] = i;
+        }
+        // 测量遍历字典的时间
+        Stopwatch sw2 = Stopwatch.StartNew();
+        long sum2 = 0;
+        foreach (var kvp in dictionary)
+        {
+            sum += kvp.Value;
+        }
+        sw2.Stop();
+        Debug.LogWarning($"遍历字典耗时: {sw2.ElapsedMilliseconds} ms");
     }
 }
